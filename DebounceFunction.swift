81997fd5bb374fa9772ddc29e27545fed876e05f
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2025-01-20 21:12:56 +0330

    Create DebounceFunction.swift

---
+++ DebounceFunction.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class Debouncer {
+    private let delay: TimeInterval
+    private var timer: Timer?
+    
+    init(delay: TimeInterval) {
+        self.delay = delay
+    }
+    
+    func run(action: @escaping () -> Void) {
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: delay, repeats: false) { _ in
+            action()
+        }
+    }
+}
+
+// Usage:
+let debouncer = Debouncer(delay: 0.5)
+debouncer.run {
+    print("Action executed!")
+}
